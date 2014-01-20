c1c836a8630b13ce3035fa6c92cc738ad0f20c4d
Soren Sonderby Nielsen <Soren@Sorens-MacBook-Pro.local>
2014-01-20 21:54:58 +0100

    Meal-model is removed. Mealinformation in Serving and a direct link from day to serving instead. Admin fittings, too

--- log/admin.py
+++ log/admin.py
@@ -1,7 +1,21 @@
 from django.contrib import admin
-from log.models import Day, Food, Meal, Serving
+from log.models import Day, Food, Serving
 
 # Register your models here.
-#class DayAdmin(admin.ModelAdmin):
 
-admin.site.register(Day)
+class ServingInline(admin.TabularInline):
+  model = Serving
+  extra = 0
+
+class DayAdmin(admin.ModelAdmin):
+  fieldsets = [
+        ('Date',               {'fields': ['day']}),
+        ('Calories', {'fields': ['max_cal']}),
+    ]
+  readonly_fields = ['cals']
+  list_display = ('day', 'max_cal', 'cals')
+  inlines = [ServingInline]
+
+admin.site.register(Food)
+admin.site.register(Serving)
+admin.site.register(Day, DayAdmin)
--- log/models.py
+++ log/models.py
@@ -5,11 +5,48 @@ class Day(models.Model):
   day = models.DateField()
   max_cal = models.IntegerField(default = 0)
 
+  def cals(self):
+    servings = Serving.objects.filter(day = self)
+    return reduce(lambda acc, serv: acc + serv.cals(), servings, 0)
+  cals.short_description = "Calories"
+  cals.admin_order_field = 'self'
+
+  def delta_cals(self):
+    return self.max_cal - self.cals()
+
   def __unicode__(self):
     return str(self.day)
 
-class Meal(models.Model):
+
+
+# class Meal(models.Model):
+#   day = models.ForeignKey(Day)
+
+#   def cals(self):
+#     food_in_meal = Serving.objects.filter(meal = self)
+#     return reduce(lambda acc, serv: acc + serv.cals(), food_in_meal, 0)
+
+#   def __unicode__(self):
+#     return str(self.day.day) + ' - ' + self.meal
+
+
+
+class Food(models.Model):
+  text = models.CharField(max_length = 255)
+  energy = models.IntegerField() # kcal/100 g
+  protein = models.DecimalField(max_digits = 3, decimal_places = 1) # g/100g
+  carbo = models.DecimalField(max_digits = 3, decimal_places = 1) # g/100g
+  fat = models.DecimalField(max_digits = 3, decimal_places = 1) # g/100g
+
+  def __unicode__(self):
+    return self.text
+
+
+
+class Serving(models.Model):
   day = models.ForeignKey(Day)
+  food = models.ForeignKey(Food)
+  amount = models.IntegerField(default = 0) # grams
   MEAL_CHOICE = (
     ('BF', 'Breakfast'),
     ('PN', 'Pre-noon'),
@@ -20,24 +57,8 @@ class Meal(models.Model):
   )
   meal = models.CharField(max_length = 2, choices = MEAL_CHOICE)
 
-  def __unicode__(self):
-    return str(self.day.day) + ' ' + self.meal
-
-class Food(models.Model):
-  text = models.CharField(max_length = 255)
-  energy = models.IntegerField() # kcal/100 g
-  protein = models.DecimalField(max_digits = 3, decimal_places = 1)
-  carbo = models.DecimalField(max_digits = 3, decimal_places = 1)
-  fat = models.DecimalField(max_digits = 3, decimal_places = 1)
+  def cals(self):
+    return int((self.food.energy / 100.0) * self.amount)
 
   def __unicode__(self):
-    return self.text
-
-
-class Serving(models.Model):
-  meal = models.ForeignKey(Meal)
-  food = models.ForeignKey(Food)
-  amount = models.IntegerField(default = 0) # grams
-
-  def __unicode__(self):
-    return self.amount + " g. " + self.food.text
+    return "%d g. %s" % (self.amount, self.food.text)
