851a9cb90fd7ea03d338e4f6fab8fd09df182090
Søren Sønderby Nielsen <ssni@itu.dk>
2014-02-08 23:13:08 +0100

    Heroku deploy

--- foodlog/wsgi.py
+++ foodlog/wsgi.py
@@ -12,3 +12,11 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "foodlog.settings")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
+
+
+#HEROKU
+from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
+
+application = Cling(get_wsgi_application())
+# HEROKU
