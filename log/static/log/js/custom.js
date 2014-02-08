b5fc256adf235f824cd49d024b7da0a1ca76e396
Søren Sønderby Nielsen <ssni@itu.dk>
2014-02-08 17:41:19 +0100

    Optimizaitons to mobile. Submit on edit serving. UI optimizations.

--- log/static/log/js/custom.js
+++ log/static/log/js/custom.js
@@ -7,6 +7,11 @@
  **/
 (function(a){(jQuery.browser=jQuery.browser||{}).mobile=/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4));})(navigator.userAgent||navigator.vendor||window.opera);
 
+$(function() {
+  if(jQuery.browser.mobile) {
+    $('#collapse-calendar').collapse('hide');
+  }
+});
 
 // Update serving-button functionality
 function updateServingButtons() {
@@ -18,6 +23,16 @@ function updateServingButtons() {
       $(this).find('.btn-group').css('visibility', 'hidden');
     });
   }
+
+  $('.remove-serving-button').click( function() {
+    var serving = $(this).closest('tr');
+    var serving_id = serving.attr('id');
+    var url = document.URL + 'serving/remove/';
+    $.post(url, {serving_id: serving_id}).done( function(data) {
+      serving.remove();
+      updateDayInfo();
+    });
+  });
 }
 
 // Show buttons on meal and servings when hovering above it. Only on desktop.
@@ -54,20 +69,6 @@ $.ajaxSetup({
             xhr.setRequestHeader("X-CSRFToken", $.cookie('csrftoken'));
         }
     }
-});
-
-// Remove serving and update meal table
-$(function() {
-  $('.remove-serving-button').click( function() {
-    console.log("REMOVE");
-    var serving = $(this).closest('tr');
-    var serving_id = serving.attr('id');
-    var url = document.URL + 'serving/remove/';
-    $.post(url, {serving_id: serving_id}).done( function(data) {
-      serving.remove();
-      updateDayInfo();
-    });
-  });
 });
 
 // Check state of add serving form and activate Add if fitting
@@ -156,7 +157,7 @@ var bar_options = {
 // Updates day and mealsubtotals
 function updateDayInfo() {
   var url = document.URL + 'info/';
-  $.post(url, {}).done(function(data) {
+  $.get(url).done(function(data) {
     // Update total
     $('#cals').text(data.cals);
 
@@ -175,7 +176,6 @@ function updateDayInfo() {
       { data:[[Math.round(data.fat), data.fat]], color: fat_color }
     ];
     bar_options['yaxis']['max'] = Math.max(data.protein, data.carbo, data.fat) + 10;
-    bar_options['yaxis']['min'] = Math.min(data.protein, data.carbo, data.fat) - 10;
     $.plot('#macro-nutrients-barchart', bar_data, bar_options);
 
     // Update nutrient-table
@@ -204,9 +204,10 @@ $(function() {
     todayHighlight: true
   });
   calendar.on('changeDate', function(e){
-    window.location = '/foodlog/' + e.format() + '/';
+    window.location = '/foodlog/' + e.format();
   });
   calendar.datepicker('update', chosen_date);
+  $('.datepicker-inline').addClass('center-block');
 
 
   updateDayInfo();
